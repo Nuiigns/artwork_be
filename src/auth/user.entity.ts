45dc1d241d898fbe32a43143e52c3b5881f86f16
Nuiigns <wathanyuok@gmail.com>
2024-08-04 12:04:32 +0700

    generate auth module

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,27 @@
+import {Entity, Column, ObjectIdColumn} from 'typeorm';
+
+@Entity({name: "user"})
+export class User {
+    @ObjectIdColumn()
+    _id: string;
+
+    @Column()
+    firstname: string;
+    
+    @Column()
+    lastname: string;
+    
+    @Column()
+    tel: string;
+
+    @Column()
+    email: string;
+
+    
+    @Column({default: null})
+    createdAt: string;
+
+        
+    @Column({default: null})
+    createdBy: string;
+}
