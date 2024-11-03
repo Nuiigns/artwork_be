56f9cec9fc18e4dde38558c4ce24853d642a4fce
Nuiigns <wathanyuok@gmail.com>
2024-11-03 09:51:03 +0700

    vercel

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,12 @@ import { Controller, Post, Get, Body, UseGuards } from '@nestjs/common';
 import { User } from './user.entity';
 import { AuthService } from './auth.service';
 import { Authorize } from 'src/authorize.guard';
-
+ 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
-    @Post()
+    @Post('register')
     async register(@Body() user: User) {
         const register = await this.authService.register(user);
         return register;
@@ -19,14 +19,14 @@ export class AuthController {
         const logIn = await this.authService.login(user);
         return logIn;
     }
-   
-   
+
+
     @Get()
     @UseGuards(Authorize)
-   async getAllUserData()
-    {return await this.authService.getAllUserData();
-    
-   }
+    async getAllUserData() {
+        return await this.authService.getAllUserData();
+
+    }
 }
 
-    
+
--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,22 +1,29 @@
 import {Entity, Column, ObjectIdColumn} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+
 
 @Entity({name: "user"})
 export class User {
     @ObjectIdColumn()
     _id: string;
 
+    @ApiProperty()
     @Column()
     firstname: string;
     
+    @ApiProperty()
     @Column()
     lastname: string;
     
+    @ApiProperty()
     @Column()
     tel: string;
 
+    @ApiProperty()
     @Column()
     email: string;
 
+    @ApiProperty()
     @Column()
     password: string;
 
